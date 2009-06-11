328893f9572c325b4b6b0d05e8b4ee05acf42945
James Coglan <jcoglan@googlemail.com>
2009-06-11 12:51:09 +0100

    Spruce up the example a little bit.

--- test/ojay-conf.js
+++ test/ojay-conf.js
@@ -2,7 +2,7 @@ JS.Packages(function() { with(this) {
         
     file('http://cdn.othermedia.com/ojay/0.4.0/core-min.js')
         .provides("Ojay", "Ojay.HTML")
-        .requires('YAHOO', 'JS.Class', 'JS.MethodChain', 'JS.Observable', 'JS.State');
+        .requires('YAHOO', 'JS.Class', 'JS.Observable', 'JS.State', 'JS.MethodChain');
     
     file('http://cdn.othermedia.com/ojay/0.4.0/pkg/overlay-min.js')
         .provides("Ojay.ContentOverlay")
