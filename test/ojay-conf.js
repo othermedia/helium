956029fa67ba73c5b63dc68cf21219898deaad65
James Coglan <jcoglan@googlemail.com>
2009-06-11 12:31:48 +0100

    Test file adjustments.

---
+++ test/ojay-conf.js
@@ -0,0 +1,14 @@
+JS.Packages(function() { with(this) {
+        
+    file('http://cdn.othermedia.com/ojay/0.4.0/core-min.js')
+        .provides("Ojay", "Ojay.HTML")
+        .requires('YAHOO', 'JS.Class', 'JS.MethodChain', 'JS.Observable', 'JS.State');
+    
+    file('http://cdn.othermedia.com/ojay/0.4.0/pkg/overlay-min.js')
+        .provides("Ojay.ContentOverlay")
+        .requires('Ojay');
+    
+    file('http://cdn.othermedia.com/ojay-yui/2.7.0.js')
+        .provides('YAHOO');
+}});
+
