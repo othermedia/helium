15d54e34306c6af28236a971d762cdbcbb93a4f5
James Coglan <jcoglan@googlemail.com>
2009-09-14 12:52:01 +0100

    Comments and some structural changes to the web frontend.

--- web/app.rb
+++ web/app.rb
@@ -9,19 +9,29 @@ LIB_DIR  = 'lib'
 CONFIG   = File.join(ROOT_DIR, 'deploy.yml')
 CUSTOM   = File.join(ROOT_DIR, 'custom.js')
 
+# Returns the data structure contained in the app's deploy.yml file.
 def project_config
   File.file?(CONFIG) ? YAML.load(File.read(CONFIG)) : {}
 end
 
+# Generic handler for displaying editable files requested using GET.
+def view_file(name)
+  @projects = project_config
+  @action   = name.to_s
+  @file     = Kernel.const_get(name.to_s.upcase)
+  @contents = File.file?(@file) ? File.read(@file) : ''
+  erb :edit
+end
+
 ## GET /
-##
+## Home page -- just loads the project list and renders.
 get '/' do
   @projects = project_config
   erb :index
 end
 
 ## POST /deploy
-##
+## Deploys all selected projects and renders a list of log messages.
 post '/deploy' do
   deployer = Helium::Deployer.new(ROOT_DIR, LIB_DIR)
   logger   = Logger.new
@@ -41,17 +51,10 @@ post '/deploy' do
 end
 
 ## GET /config
-##
-get '/config' do
-  @projects = project_config
-  @action   = 'config'
-  @file     = CONFIG
-  @contents = File.file?(@file) ? File.read(@file) : ''
-  erb :config
-end
+get('/config') { view_file :config }
 
 ## POST /config
-##
+## Save changes to the configuration file, making sure it validates as YAML.
 post '/config' do
   @action   = 'config'
   @file     = CONFIG
@@ -63,30 +66,25 @@ post '/config' do
     @error = 'File not saved: invalid YAML'
   end
   @projects = project_config
-  erb :config
+  erb :edit
 end
 
 ## GET /custom
-##
-get '/custom' do
-  @projects = project_config
-  @action   = 'custom'
-  @file     = CUSTOM
-  @contents = File.file?(@file) ? File.read(@file) : ''
-  erb :config
-end
+get('/custom') { view_file :custom }
 
 ## POST /custom
-##
+## Save changes to the custom loaders file.
 post '/custom' do
   @projects = project_config
   @action   = 'custom'
   @file     = CUSTOM
   @contents = params[:contents]
   File.open(@file, 'w') { |f| f.write(@contents) }
-  erb :config
+  erb :edit
 end
 
+# Class to pick up log messages from the build process so we can display
+# them to the user on completion.
 class Logger
   attr_reader :messages
   def initialize
