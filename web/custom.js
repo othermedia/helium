8b98651ba7e6f22f5b8c52af2624d6fe74c1c97b
James Coglan <jcoglan@googlemail.com>
2009-09-15 12:29:23 +0100

    Provide an executable for installing the web app to a local directory ready for serving through Rack. Also provide default contents for deploy.yml and custom.js.

---
+++ web/custom.js
@@ -0,0 +1,9 @@
+loader(function(cb) {
+    var url = 'http://www.google.com/jsapi?key=' + Helium.GOOGLE_KEY;
+    load(url, cb);
+})  .provides('google.load');
+
+loader(function(cb) { google.load('maps', '2.x', {callback: cb}) })
+    .provides('GMap2', 'GClientGeocoder')
+    .requires('google.load');
+
