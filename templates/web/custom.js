5fdfbb3fc4ba0eba538efa25de330a8bd8528d0e
James Coglan <jcoglan@googlemail.com>
2009-09-18 12:09:38 +0100

    Add Google Analytics loaders to the web template and update some sample code.

--- templates/web/custom.js
+++ templates/web/custom.js
@@ -1,8 +1,36 @@
+/**
+ * Loads the `_gat` object for Google Analytics
+ **/
 loader(function(cb) {
-    var url = 'http://www.google.com/jsapi?key=' + Helium.GOOGLE_KEY;
+    var gaJsHost = ("https:" == document.location.protocol) ? "https://ssl." : "http://www.";
+    load(gaJsHost + 'google-analytics.com/ga.js', cb);
+})  .provides('_gat');
+
+/**
+ * Loads a `pageTracker` object for making Analytics API calls.
+ * Requires `Helium.GOOGLE_ANALYTICS_ID` to be set.
+ **/
+loader(function(cb) {
+    try {
+        window.pageTracker = _gat._getTracker(Helium.GOOGLE_ANALYTICS_ID);
+    } catch (err) {}
+})  .provides('pageTracker')
+    .requires('_gat');
+
+/**
+ * Loads the `google.load` function, required to load other
+ * parts of the Google API. Requires `Helium.GOOGLE_API_KEY`
+ * to be set beforehand.
+ **/
+loader(function(cb) {
+    var url = 'http://www.google.com/jsapi?key=' + Helium.GOOGLE_API_KEY;
     load(url, cb);
 })  .provides('google.load');
 
+/**
+ * Loads the Google Maps API. Requires `Helium.GOOGLE_API_KEY`
+ * to be set beforehand.
+ **/
 loader(function(cb) { google.load('maps', '2.x', {callback: cb}) })
     .provides('GMap2', 'GClientGeocoder',
               'GEvent', 'GLatLng', 'GMarker')
